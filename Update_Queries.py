238a2f025254aed7e82bc2928343a53a412936b5
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-08-11 06:52:14 -0500

    Update Update_Queries.py

--- Update_Queries.py
+++ Update_Queries.py
@@ -34,19 +34,171 @@ for row in result:
 print(role)
 cur.execute("INSERT INTO USER_ACCOUNT VALUES (%s, %s, %s, %s)", (total_num, name, cell, role))
 
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
+
 # 3.2
-#cur.execute("INSERT INTO USER_ROLE VALUES ('coach', 'los deportes')")
+role = input("Enter role name: ")
+desc = input("Enter role description: ")
+cur.execute("INSERT INTO USER_ROLE VALUES (%s, %s)", (role, desc))
+
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
 
 # 3.3
-#cur.execute("INSERT INTO TABLE_EX VALUES ('10', 'Table_11')")
+cur.execute ("SELECT * FROM USER_ACCOUNT")
+result = cur.fetchall()
+
+print("ID #	Name		Phone		My Role")
+print("\n")
+for row in result:
+	print (row[0],"\t",row[1],"\t",row[2],"\t", row[3])
+
+owwwn = int(input("Pick an owner: "))
+nam_tab = input("Enter name of table: ")
+cur.execute("INSERT INTO TABLE_EX VALUES (%s, %s)", (owwwn, nam_tab))
+
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
 
 # 3.4
-#cur.execute("INSERT INTO PRIVILEGE VALUES ('REFERENCES', 'ACCOUNT')")
+priv_name = input("Enter privilege name: ")
+priv_type = ["ACCOUNT", "RELATIONAL"]
+print("Enter privilege type \n Press 0 for ACCOUNT \n Press 1 for RELATIONAL")
+priv_choice = int(input("What is your choice? "))
+
+cur.execute("INSERT INTO PRIVILEGE VALUES (%s, %s)", (priv_name, priv_type[priv_choice]))
+
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
 
 # 3.5
-#cur.execute("INSERT INTO USER_ACCOUNT VALUES ('22', 'D.Beckham', '888-070-8888', 'coach')")
+#cur.execute("UPDATE INTO USER_ACCOUNT VALUES ('22', 'D.Beckham', '888-070-8888', 'coach')")
 
 # 3.6
-#cur.execute("INSERT INTO HAS_PRIV VALUES ('admin', 'REFERENCES')")
+cur.execute ("SELECT * FROM USER_ROLE")
+result = cur.fetchall()
+
+print("Role Name		Description")
+print("\n")
+count = 0
+for row in result:
+	#print("Role Name	Description")
+	#for item in row:
+	print (count, row[0].ljust(15),"\t", row[1].ljust(15))
+	count += 1
+role_priv = int(input("Pick a role: "))
+role_choi = ""
+count = 0
+for row in result:
+	print(count)
+	if count == role_priv:
+		role_choi = row[0]
+		break
+	count += 1
+print("role ",role_choi)
+
+cur.execute ("SELECT * FROM PRIVILEGE")
+result = cur.fetchall()
+
+print("Priv Name 		Priv Type")
+print("\n")
+count = 0
+for row in result:
+	print (count, row[0].ljust(15),"       ",row[1].ljust(15))
+	count += 1
+priv_choi = int(input("Pick a privilege: "))
+privvv = ""
+count = 0
+for row in result:
+	if count == priv_choi:
+		privvv = row[0]
+		break
+	count += 1
+print("priv ", privvv)
+
+
+cur.execute("INSERT INTO HAS_PRIV VALUES (%s, %s)", (role_choi, privvv))
+
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
 
 # 3.7
+
+# 3.8
+
+nombre_role = input("Enter role name: ")
+
+cur.execute("SELECT * FROM HAS_PRIV WHERE roleName = %s", nombre_role)
+
+result = cur.fetchall()
+
+for row in result:
+	print(row)
+
+nombre_user = input("Enter user name: ")
+cur.execute("SELECT name, my_role, privName FROM HAS_PRIV, USER_ACCOUNT WHERE name=%s AND my_role=roleName", nombre_user)
+result = cur.fetchall()
+
+for row in result:
+	print(row)
+
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
+
+# 3.8
+
+nombre_role = input("Enter role name: ")
+
+cur.execute("SELECT * FROM HAS_PRIV WHERE roleName = %s", nombre_role)
+
+result = cur.fetchall()
+
+for row in result:
+	print(row)
+
+nombre_user = input("Enter user name: ")
+cur.execute("SELECT name, my_role, privName FROM HAS_PRIV, USER_ACCOUNT WHERE name=%s AND my_role=roleName", nombre_user)
+result = cur.fetchall()
+
+for row in result:
+	print(row)
+
+print("\n")
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+print("\n")
+
+# 3.9
+cur.execute ("SELECT * FROM USER_ROLE")
+result = cur.fetchall()
+
+print("Role Name		Description")
+print("\n")
+for row in result:
+	#print("Role Name	Description")
+	#for item in row:
+	print (row[0].ljust(15),"\t", row[1].ljust(15))
+
+nombre_role = input("Enter role name to revoke privileges: ")
+
+cur.execute("SELECT * FROM HAS_PRIV WHERE roleName = %s", nombre_role)
+
+result = cur.fetchall()
+
+for row in result:
+	print(row)
+
+del_priv = input("Enter priv to revoke: ")
+cur.execute("DELETE FROM HAS_PRIV WHERE privName = %s", del_priv)
+
+
+
+
+
+
